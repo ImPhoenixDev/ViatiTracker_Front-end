cd4983b760efd7cb07afb957048d89ac935717bc
Guillermo Loza <iguillermoloza@gmail.com>
2023-07-20 21:37:26 -0600

    chore(axios): axios default config with auth

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -4,8 +4,11 @@ import Login from '../components/login/login'
 import Dashboard from '@/components/dashboard/Dashboard'
 import PrivateRoute from '@/components/privateRoute/PrivateRoute'
 import CreateExpense from '@/components/createExpense/CreateExpense'
+import axios from 'axios'
 
 const App = () => {
+  axios.defaults.withCredentials = true
+
   return (
     <Router>
       <Login path="/" />
