e8d0e65f160cfa32ca3229582d5ec477bc7fad3a
Guillermo Loza <iguillermoloza@gmail.com>
2023-07-31 21:15:31 -0600

    fix(dashboard): now dates in the table are formated

--- src/components/expenseRow/ExpenseRow.tsx
+++ src/components/expenseRow/ExpenseRow.tsx
@@ -18,6 +18,23 @@ export default function ExpenseRow(expense: ExpenseRowProps) {
     statusClass = `text-lightGray`
   }
 
+  function formatDate(dateInput) {
+    let date
+
+    if (typeof dateInput === `string` && dateInput.includes(`/`)) {
+      const [day, month, year] = dateInput.split(`/`)
+      date = new Date(year, month - 1, day)
+    } else {
+      date = new Date(dateInput)
+    }
+
+    const dd = String(date.getDate()).padStart(2, `0`)
+    const mm = date.toLocaleString(`es-MX`, { month: `short` }).replace(`.`, ``)
+    const yyyy = date.getFullYear()
+
+    return dd + `/` + mm + `/` + yyyy
+  }
+
   return (
     <tr
       className="flex border-b border-lightGray py-2 cursor-pointer"
@@ -37,7 +54,9 @@ export default function ExpenseRow(expense: ExpenseRowProps) {
       <td className="h-12 w-3/6 p-0 m-0">
         <div className="justify-center items-end flex flex-col">
           <span className={`${statusClass} text-sm`}>{expense.status}</span>
-          <span className="text-lightGray text-sm">{expense.date}</span>
+          <span className="text-lightGray text-sm">
+            {formatDate(expense.date)}
+          </span>
         </div>
       </td>
     </tr>
